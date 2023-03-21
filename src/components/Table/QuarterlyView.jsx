446347b79d9e4d6d45f4d1d8be127d05c7874476
Erik <Mr.erik.egliens@gmail.com>
2023-03-21 20:21:50 +0200

    task removing and creating logic

--- src/components/Table/QuarterlyView.jsx
+++ src/components/Table/QuarterlyView.jsx
@@ -4,14 +4,15 @@ import { UserContext } from "../../utils/useContext";
 import { nanoid } from "nanoid";
 
 const QuarterlyView = () => {
-  const {tasks, createNewTask, updateTask} = useContext(UserContext);
+  const { tasks, createNewTask, updateTask, deleteTask } =
+    useContext(UserContext);
 
   const todaysQuarter = Math.floor((new Date(2022, 0, 1).getMonth() + 3) / 3);
   const [currentQuarter, setCurrentQuarter] = useState(todaysQuarter);
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
 
-
-  function getISO8601_week_no(day) { // day is monday
+  function getISO8601_week_no(day) {
+    // day is monday
     day.setDate(day.getDate() + 3); // move day from monday to thursday
     const firstThursday = day.valueOf(); //get timestamp
     const firstJanuary = new Date(day.setMonth(0, 1)); // set day to 1st january
@@ -78,7 +79,7 @@ const QuarterlyView = () => {
         });
       }
       headers.push(
-        <th className="p-0" colSpan={colSpanSize} key={monthIndex}>
+        <th className="p-0" colSpan={colSpanSize} key={nanoid()}>
           <table style={{ tableLayout: "fixed", width: "100%" }}>
             <thead>
               <tr>
@@ -92,8 +93,8 @@ const QuarterlyView = () => {
                 {weeks.map((item) => {
                   if (new Date(item.start).getMonth() === monthIndex) {
                     return (
-                      <td className="text-center" key={item.number}> 
-                        {item.number}  
+                      <td className="text-center" key={nanoid()}>
+                        {item.number}
                       </td>
                     ); // render week number
                   }
@@ -117,30 +118,59 @@ const QuarterlyView = () => {
     for (let week of weeks) {
       const cellClassName =
         week.start <= endDate && week.end >= startDate ? "bg-success" : "";
-      cells.push(
-        <td key={`${id}-${week.number}`} className={cellClassName}></td>
-      );
+      cells.push(<td key={nanoid()} className={cellClassName}></td>);
     }
     return (
-      <tr key={id}>
-        <td title={`Description of ${name}`}>{name}</td>
-        <td> 
+      <tr key={nanoid()}>
+        <td title={`Description of ${name}`}>
+          <div className="d-flex gap-1 align-items-center justify-content-between">
+            <p className="m-0">{name}</p>
+            <button
+              onClick={() => deleteTask(id)}
+              className="border-0 bg-transparent p-0"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                fill="#000000"
+                version="1.1"
+                id="Capa_1"
+                width="15px"
+                height="15px"
+                viewBox="0 0 482.428 482.429"
+                xmlSpace="preserve"
+              >
+                <g>
+                  <g>
+                    <path d="M381.163,57.799h-75.094C302.323,25.316,274.686,0,241.214,0c-33.471,0-61.104,25.315-64.85,57.799h-75.098    c-30.39,0-55.111,24.728-55.111,55.117v2.828c0,23.223,14.46,43.1,34.83,51.199v260.369c0,30.39,24.724,55.117,55.112,55.117    h210.236c30.389,0,55.111-24.729,55.111-55.117V166.944c20.369-8.1,34.83-27.977,34.83-51.199v-2.828    C436.274,82.527,411.551,57.799,381.163,57.799z M241.214,26.139c19.037,0,34.927,13.645,38.443,31.66h-76.879    C206.293,39.783,222.184,26.139,241.214,26.139z M375.305,427.312c0,15.978-13,28.979-28.973,28.979H136.096    c-15.973,0-28.973-13.002-28.973-28.979V170.861h268.182V427.312z M410.135,115.744c0,15.978-13,28.979-28.973,28.979H101.266    c-15.973,0-28.973-13.001-28.973-28.979v-2.828c0-15.978,13-28.979,28.973-28.979h279.897c15.973,0,28.973,13.001,28.973,28.979    V115.744z" />
+                    <path d="M171.144,422.863c7.218,0,13.069-5.853,13.069-13.068V262.641c0-7.216-5.852-13.07-13.069-13.07    c-7.217,0-13.069,5.854-13.069,13.07v147.154C158.074,417.012,163.926,422.863,171.144,422.863z" />
+                    <path d="M241.214,422.863c7.218,0,13.07-5.853,13.07-13.068V262.641c0-7.216-5.854-13.07-13.07-13.07    c-7.217,0-13.069,5.854-13.069,13.07v147.154C228.145,417.012,233.996,422.863,241.214,422.863z" />
+                    <path d="M311.284,422.863c7.217,0,13.068-5.853,13.068-13.068V262.641c0-7.216-5.852-13.07-13.068-13.07    c-7.219,0-13.07,5.854-13.07,13.07v147.154C298.213,417.012,304.067,422.863,311.284,422.863z" />
+                  </g>
+                </g>
+              </svg>
+            </button>
+          </div>
+        </td>
+        <td className="p-0">
           <input
-              type="date"
-              placeholder="Start date"
-              onChange={() => updateTask(e, id) }
-              name="start"
-              value={start}
+            className="w-100"
+            type="date"
+            placeholder="Start date"
+            onChange={(e) => updateTask(e, id)}
+            name="start"
+            value={start}
           />
         </td>
-        <td>
-        <input
-              type="date"
-              placeholder="End date"
-              onChange={(e) => updateTask(e, id)}
-              name="end"
-              value={end}
-              id={`endDate-${id}`}
+        <td className=" p-0">
+          <input
+            className="w-100"
+            type="date"
+            placeholder="End date"
+            onChange={(e) => updateTask(e, id)}
+            name="end"
+            value={end}
+            id={`endDate-${id}`}
           />
         </td>
         {cells}
@@ -180,12 +210,14 @@ const QuarterlyView = () => {
             onClick={handleNextQuarterClick}
           >{`Next 3 Months >`}</button>
         </div>
-        <button 
+        <button
           disabled={tasks && tasks.length == 10}
           onClick={createNewTask}
           className="text-dark fw-semibold btn btn-success btn-lg me-1 rounded-0"
         >
-          {tasks && tasks.length != 10 ? 'Add task +' : "Maximum Number of Tasks Allowed Has Been Reached"} 
+          {tasks && tasks.length != 10
+            ? "Add task +"
+            : "Maximum Number of Tasks Allowed Has Been Reached"}
         </button>
       </div>
       <Table bordered hover style={{ tableLayout: "fixed", width: "100%" }}>
--- src/utils/useContext.jsx
+++ src/utils/useContext.jsx
@@ -1,56 +1,55 @@
+import { useState, useEffect, createContext } from "react";
 import { nanoid } from "nanoid";
 const UserContext = createContext();
 
 function AppContextProvider(props) {
   const [tasks, setTasks] = useState(
-    () => JSON.parse(localStorage.getItem("tasks")) || 
-    [{
-      name: "Task 1",
-      start: "2022-12-31",
-      end: "2023-02-05",
-      id: 1
-    }]
+    () =>
+      JSON.parse(localStorage.getItem("tasks")) || [
+        {
+          name: "Task 1",
+          start: "2022-12-31",
+          end: "2023-02-05",
+          id: 1,
+        },
+      ]
   );
-
+  const [tasksCounter, setTasksCounter] = useState(tasks.length);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
   function createNewTask() {
-    const year = new Date().getFullYear()
-    const month = new Date().getMonth()
-    const day = new Date().getDate()
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth();
+    const day = new Date().getDate();
     const newTask = {
-      name: `Task ${tasks.length +1}`,
-      start: `${year}-${month+1}-${day}`,
-      end: `${year}-${month+1}-${day + 1}`,
-      id: tasks.length + 1,
+      name: `Task ${tasksCounter}`,
+      start: `${year}-${month + 1}-${day}`,
+      end: `${year}-${month + 1}-${day + 1}`,
+      id: nanoid(),
     };
+    setTasksCounter((prevCount) => prevCount + 1);
     setTasks((prevTasks) => [newTask, ...prevTasks]);
   }
-console.log(tasks);
   function updateTask(event, id) {
     const { name, value } = event.target;
-    console.log(name, value, id);
-    setTasks(prevTasks => {
-      prevTasks.map((prevTask) => {
-        console.log(prevTask.id);
-        prevTask.id === id ? 
-        {...prevTask, [name]: value} : 
-        {...prevTask}
-      })
-    })
-}
-  function deleteTask(event, taskId) {
-    event.stopPropagation();
-    const filteredTasks = tasks.filter(task => task.id !== taskId);
+    setTasks((prevTasks) =>
+      prevTasks.map((prevTask) =>
+        prevTask.id === id ? { ...prevTask, [name]: value } : prevTask
+      )
+    );
+  }
+  function deleteTask(taskId) {
+    const filteredTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(filteredTasks);
   }
 
-
   return (
-    <UserContext.Provider value={{ createNewTask, updateTask, tasks }}>
+    <UserContext.Provider
+      value={{ createNewTask, updateTask, deleteTask, tasks }}
+    >
       {props.children}
     </UserContext.Provider>
   );
